0b14554e1f550d9fef85fb2711360c447928f69a
Ultra-Code <mega.alpha100@gmail.com>
2022-01-13 18:24:30 +0000

    Implement a stack backed by an ArrayList

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const linked_list = @import("linked_list.zig");
+const stack = @import("stack.zig");
 
 test {
     std.testing.refAllDecls(@This());
---
+++ src/stack.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+//NOTE: Also implement a ComptimeStack backed by a static array
+fn Stack(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        data: std.ArrayList(T),
+        size: usize = 0,
+
+        pub fn init(allocator: std.mem.Allocator) Self {
+            return .{ .data = std.ArrayList(T).init(allocator) };
+        }
+
+        pub fn push(self: *Self, value: T) void {
+            self.data.append(value) catch |err| {
+                std.debug.panic("{s} occured", .{@errorName(err)});
+                std.process.exit(1);
+            };
+            self.size += 1;
+        }
+
+        pub fn pop(self: *Self) T {
+            self.size -= 1;
+            return self.data.pop();
+        }
+
+        pub fn isEmpty(self: Self) bool {
+            return if (self.size == 0) true else false;
+        }
+
+        pub fn top(self: Self) !T {
+            if (self.isEmpty()) {
+                return error.StackIsEmpty;
+            }
+            return self.data.items[self.size - 1];
+        }
+
+        pub fn deinit(self: Self) void {
+            self.data.deinit();
+        }
+    };
+}
+
+const expect = std.testing.expect;
+test "Stack" {
+    var stack = Stack(u8).init(std.testing.allocator);
+    defer stack.deinit();
+
+    stack.push(1);
+    try expect((try stack.top()) == 1);
+    stack.push(2);
+    stack.push(3);
+    try expect((try stack.top()) == 3);
+    _ = stack.pop();
+    try expect((try stack.top()) == 2);
+    try expect(stack.size == 2);
+    try expect(stack.isEmpty() == false);
+}
