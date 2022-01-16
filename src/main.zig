114d9aad51f81a42789e31cea374e6ac7fb6f086
Ultra-Code <mega.alpha100@gmail.com>
2022-01-16 05:28:26 +0000

    import queue for testing

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const linked_list = @import("linked_list.zig");
 const stack = @import("stack.zig");
+const queue = @import("queue.zig");
 
 test {
     std.testing.refAllDecls(@This());
