e89bcf10415f5a4bfb6219aa2c746ab243b0f00c
Ultra-Code <mega.alpha100@gmail.com>
2022-01-30 01:38:06 +0000

    Update build.zig to the new root file algods.zig

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("libalgods", "src/main.zig");
+    const lib = b.addStaticLibrary("libalgods", "src/algods.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    const main_tests = b.addTest("src/main.zig");
+    const main_tests = b.addTest("src/algods.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
